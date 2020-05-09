b968ba7db268772d266c4e402679caf321414088
Parker <parkerjlandon@gmail.com>
2020-05-09 01:16:28 -0700

    readme

--- src/app/wikipedia-search.service.ts
+++ src/app/wikipedia-search.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 
 @Injectable()
 export class WikipediaSearchService {
   constructor(private http: HttpClient) {}
-
   search(term: string) {
-    let headers = new HttpHeaders();
-    headers.append("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
     let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + term + "&format=json"
     return this.http
       .jsonp(url, 'callback')
