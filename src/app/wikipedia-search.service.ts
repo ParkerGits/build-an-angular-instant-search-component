cf3ebce5dcf0b0a2531385ab28260b8b8b6df9fe
Parker <parkerjlandon@gmail.com>
2020-05-09 04:37:43 -0700

    Update wikipedia-search.service.ts

--- src/app/wikipedia-search.service.ts
+++ src/app/wikipedia-search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs/Observable'
+import { Observable } from 'rxjs';
 
 //application wide shared Rx operators
 import { map, debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
