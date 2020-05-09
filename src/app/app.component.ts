f71fb037a3751782f304fdb0dc03a53793eab916
Parker <parkerjlandon@gmail.com>
2020-05-09 03:09:23 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { WikipediaSearchService } from "./wikipedia-search.service";
 import { Subject } from "rxjs";
 
 //application wide shared Rx operators
-import { map, debounceTime, distinctUntilChanged, mergeMap } from "rxjs/operators";
+import { map, debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -16,7 +16,7 @@ export class AppComponent {
         this.term$.pipe(
           debounceTime(400),
           distinctUntilChanged(),
-          mergeMap(term => this.service.search(term)),
+          switchMap(term => this.service.search(term)),
         ).subscribe(results => this.items = results);
   }
 }
