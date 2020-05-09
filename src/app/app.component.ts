bd48bd3410cbd1c4e81ba017eb99096a5e5f4bd1
Parker <parkerjlandon@gmail.com>
2020-05-09 03:36:19 -0700

    lesson 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 import { WikipediaSearchService } from "./wikipedia-search.service";
 import { Subject } from "rxjs";
 
-//application wide shared Rx operators
-import { map, debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
-
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html"
@@ -13,10 +10,6 @@ export class AppComponent {
   items: Array<string>;
   term$ = new Subject<string>();
   constructor(private service: WikipediaSearchService) {
-        this.term$.pipe(
-          debounceTime(400),
-          distinctUntilChanged(),
-          switchMap(term => this.service.search(term)),
-        ).subscribe(results => this.items = results);
+    this.service.search(this.term$).subscribe(results => this.items=results)
   }
 }
--- src/app/wikipedia-search.service.ts
+++ src/app/wikipedia-search.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { Observable } from 'rxjs/Observable'
+
+//application wide shared Rx operators
+import { map, debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
 
 @Injectable()
 export class WikipediaSearchService {
   constructor(private http: HttpClient) {}
-  search(term: string) {
+
+  search(terms: Observable<string>, debounceMs = 400) {
+      return terms.pipe(
+        debounceTime(debounceMs),
+        distinctUntilChanged(),
+        switchMap(term => this.rawSearch(term))
+      )
+  }
+
+  rawSearch(term: string) {
     let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + term + "&format=json"
     return this.http
       .jsonp(url, 'callback')
