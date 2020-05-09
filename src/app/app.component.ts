2b55948b0064b98af98c3837dd5f82fc4d4f8db7
Parker <parkerjlandon@gmail.com>
2020-05-09 02:49:37 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { WikipediaSearchService } from "./wikipedia-search.service";
 import { Subject } from "rxjs";
 
 //application wide shared Rx operators
-import { map, debounceTime } from "rxjs/operators";
+import { map, debounceTime, distinctUntilChanged } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -13,7 +13,10 @@ export class AppComponent {
   items: Array<string>;
   term$ = new Subject<string>();
   constructor(private service: WikipediaSearchService) {
-        this.term$.pipe(debounceTime(400)).subscribe(term => this.search(term));
+        this.term$.pipe(
+          debounceTime(400),
+          distinctUntilChanged(),
+        ).subscribe(term => this.search(term));
   }
 
   search(term: string) {
